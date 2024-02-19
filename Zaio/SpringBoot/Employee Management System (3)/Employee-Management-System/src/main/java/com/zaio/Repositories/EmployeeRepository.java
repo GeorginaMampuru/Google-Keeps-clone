a762610c230b49c023129166193c0d71de308510
Georgina Mampuru <66384787+GeorginaMampuru@users.noreply.github.com>
2024-02-19 21:22:11 +0200

    Delete Zaio directory

--- Zaio/SpringBoot/Employee Management System (3)/Employee-Management-System/src/main/java/com/zaio/Repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zaio.Repositories;
-
-import com.zaio.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
